80fc89bef02907d5c713559bcac710f2cf98b3db
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-12 18:16:17 +0800

    initial commit

---
+++ backend/dogadoption/src/main/java/com/example/dogadoption/DogadoptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogadoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogadoptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogadoptionApplication.class, args);
+    }
+
+}
---
+++ backend/dogadoption/src/test/java/com/example/dogadoption/DogadoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogadoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogadoptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
