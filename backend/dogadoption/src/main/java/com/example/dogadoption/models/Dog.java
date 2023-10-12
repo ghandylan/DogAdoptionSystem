d97625411ddbfb52a4feda96969a89f184cf2435
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-12 21:00:12 +0800

    created models.
    read the todo

--- backend/dogadoption/src/main/java/com/example/dogadoption/models/Dog.java
+++ backend/dogadoption/src/main/java/com/example/dogadoption/models/Dog.java
@@ -1,4 +1,147 @@
 package com.example.dogadoption.models;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    // TODO: Determine how to store images in database
+    private String picture;
+    private String name;
+    private int age;
+    private String dateOfBirth;
+    private String gender;
+    private String breed;
+    private int height;
+    private int weight;
+    private String medicalConditions;
+
+    public Dog() {
+    }
+
+    public Dog(long id, String picture, String name, int age, String dateOfBirth, String gender, String breed, int height, int weight, String medicalConditions) {
+        this.id = id;
+        this.picture = picture;
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.breed = breed;
+        this.height = height;
+        this.weight = weight;
+        this.medicalConditions = medicalConditions;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getMedicalConditions() {
+        return medicalConditions;
+    }
+
+    public void setMedicalConditions(String medicalConditions) {
+        this.medicalConditions = medicalConditions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return getId() == dog.getId() && getAge() == dog.getAge() && getHeight() == dog.getHeight() && getWeight() == dog.getWeight() && Objects.equals(getPicture(), dog.getPicture()) && Objects.equals(getName(), dog.getName()) && Objects.equals(getDateOfBirth(), dog.getDateOfBirth()) && Objects.equals(getGender(), dog.getGender()) && Objects.equals(getBreed(), dog.getBreed()) && Objects.equals(getMedicalConditions(), dog.getMedicalConditions());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPicture(), getName(), getAge(), getDateOfBirth(), getGender(), getBreed(), getHeight(), getWeight(), getMedicalConditions());
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "id=" + id +
+                ", picture='" + picture + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", gender='" + gender + '\'' +
+                ", breed='" + breed + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", medicalConditions='" + medicalConditions + '\'' +
+                '}';
+    }
 }
--- backend/dogadoption/src/main/java/com/example/dogadoption/models/Vet.java
+++ backend/dogadoption/src/main/java/com/example/dogadoption/models/Vet.java
@@ -1,5 +1,91 @@
 package com.example.dogadoption.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Vet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+    private String password;
+    private String address;
+    private String dateAndTimeCreated;
+
+    public Vet() {
+    }
+
+    public Vet(long id, String username, String password, String address, String dateAndTimeCreated) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.dateAndTimeCreated = dateAndTimeCreated;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateAndTimeCreated() {
+        return dateAndTimeCreated;
+    }
+
+    public void setDateAndTimeCreated(String dateAndTimeCreated) {
+        this.dateAndTimeCreated = dateAndTimeCreated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vet vet = (Vet) o;
+        return getId() == vet.getId() && Objects.equals(getUsername(), vet.getUsername()) && Objects.equals(getPassword(), vet.getPassword()) && Objects.equals(getAddress(), vet.getAddress()) && Objects.equals(getDateAndTimeCreated(), vet.getDateAndTimeCreated());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUsername(), getPassword(), getAddress(), getDateAndTimeCreated());
+    }
+
+    @Override
+    public String toString() {
+        return "Vet{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                ", dateAndTimeCreated='" + dateAndTimeCreated + '\'' +
+                '}';
+    }
 }
