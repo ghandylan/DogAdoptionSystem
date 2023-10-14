2297b6dcf51673b4eee51a991f185a56e03f5c13
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-14 19:12:45 +0800

    added show dogs endpoint for frontend
    read TODO for more details

---
+++ frontend/app/src/app/models/dog.ts
@@ -0,0 +1,12 @@
+export class Dog {
+  id:number = 0;
+  picture:string = "";
+  name:string = "";
+  age:number = 0;
+  dateOfBirth:string = "";
+  gender:string = "";
+  breed:string = "";
+  height:number = 0;
+  weight:number = 0;
+  medicalConditions:string = "";
+}
