e20b629badb0949f7015ddbdd09a73bf8271655b
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-26 19:01:57 +0800

    added creation of Vet entity on run

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ShowDogsComponent} from './show-dogs/show-dogs.component';
+import {LoginComponent} from "./login/login.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'dogs', pathMatch: 'full' },
   { path: 'dogs', component: ShowDogsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: UserProfileComponent}
   // TODO: Add a route for the dog detail component
 ];
 
