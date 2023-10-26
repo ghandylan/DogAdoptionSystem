d184792acd63315c888dc3043e603c62f920a620
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-26 18:12:18 +0800

    Fixed update dog

--- backend/dogadoption/src/main/java/com/example/dogadoption/controllers/DogController.java
+++ backend/dogadoption/src/main/java/com/example/dogadoption/controllers/DogController.java
@@ -7,7 +7,9 @@ import com.example.dogadoption.services.VetService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -29,7 +31,7 @@ public class DogController {
     }
 
     @PostMapping("/dog")
-    public String addDog(@RequestParam("dogImage")MultipartFile dogImage, @ModelAttribute Dog dog) {
+    public String addDog(@RequestParam("dogImage") MultipartFile dogImage, @ModelAttribute Dog dog) {
         try {
             String pictureName = dogImage.getOriginalFilename();
             String pictureType = dogImage.getContentType();
@@ -43,17 +45,53 @@ public class DogController {
 
 
             return "File saved successfully";
-        }
-
-        catch(Exception e) {
+        } catch (Exception e) {
             return "File not saved";
         }
     }
 
-    @PutMapping("dog/{id}")
-    public Dog updateDog(@PathVariable Long id, @RequestBody Dog dog) {
-        return dogService.updateDog(id, dog);
+    @PostMapping("/dog/update/{id}")
+    public String updateDog(
+            @PathVariable("id") long id, // Path variable to identify the dog by its ID
+            @RequestParam("dogImage") MultipartFile dogImage,
+            @ModelAttribute Dog dog) {
+        try {
+            // Check if the dog with the specified ID exists in the database
+
+
+            Optional<Dog> dogOptional = dogService.getDogById(id);
+
+            if (dogOptional.isPresent()) {
+                Dog existingDog = dogOptional.get();
+                // Update the dog's information, including the image if provided
+                String pictureName = dogImage.getOriginalFilename();
+                String pictureType = dogImage.getContentType();
+                byte[] fileContent = dogImage.getBytes();
+
+                // Update the dog's information
+                existingDog.setPictureName(pictureName);
+                existingDog.setPictureType(pictureType);
+                existingDog.setPicture(fileContent);
+                existingDog.setName(dog.getName()); // Update other dog fields as needed
+                existingDog.setAge(dog.getAge());
+                existingDog.setDateOfBirth(dog.getDateOfBirth());
+                existingDog.setGender(dog.getGender());
+                existingDog.setBreed(dog.getBreed());
+                existingDog.setHeight(dog.getHeight());
+                existingDog.setWeight(dog.getWeight());
+                existingDog.setMedicalConditions(dog.getMedicalConditions());
+
+                dogService.updateDog(id, existingDog);
+            }
+
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return "Dog updated successfully";
+
     }
+
 
     @DeleteMapping("dog/{id}")
     public void deleteDogById(@PathVariable Long id) {
