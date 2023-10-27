8f15eba9fbcad14c5e4ef86ce013151b468c1907
JaiTolentino <jaitolentinoo@gmail.com>
2023-10-27 16:10:06 +0800

    feat: frontend and backend connected

--- frontend/app/src/app/admin-database/admin-database.component.ts
+++ frontend/app/src/app/admin-database/admin-database.component.ts
@@ -1,27 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DogService } from '../services/dog.service';
 
 @Component({
   selector: 'app-admin-database',
   templateUrl: './admin-database.component.html',
   styleUrls: ['./admin-database.component.css']
 })
-export class AdminDatabaseComponent {
+export class AdminDatabaseComponent implements OnInit {
 
-  dogs: any;
-  selectedDog: any;
-  
-  updateDog() {
-  throw new Error('Method not implemented.');
+  dogs: any = []; // Initialize with an empty array
+  selectedDog: any = {};
+  newDog: any = {};
+
+  constructor(private dogService: DogService){}
+  ngOnInit(): void {
+    this.getDogs();
   }
-  deleteDog(arg0: any) {
-  throw new Error('Method not implemented.');
-  }
-  editDog(_t49: any) {
-  throw new Error('Method not implemented.');
-  }
-  newDog: any;
+getDogs(): void{
+  this.dogService.getDogs().subscribe(dogs => this.dogs = dogs )
+}
+
+  // Add a new dog to the list
   addDog() {
-  throw new Error('Method not implemented.');
+    this.dogs.push(this.newDog);
+    this.newDog = {}; // Clear the form after adding
   }
-  
+
+  // Delete a dog based on its ID
+  deleteDog(dogId: number) {
+    this.dogs = this.dogs.filter((dog: { id: number; }) => dog.id !== dogId);
+    this.selectedDog = {}; // Clear the selected dog
+  }
+
+  // Edit a dog
+  editDog(dog: any) {
+    this.selectedDog = { ...dog }; // Create a copy of the selected dog
+  }
+
+  // Update the selected dog
+  updateDog() {
+    const index = this.dogs.findIndex((dog: { id: any; }) => dog.id === this.selectedDog.id);
+    if (index !== -1) {
+      this.dogs[index] = { ...this.selectedDog }; // Update the dog in the list
+      this.selectedDog = {}; // Clear the selected dog
+    }
+  }
 }
--- frontend/app/src/app/show-dog/show-dog.component.ts
+++ frontend/app/src/app/show-dog/show-dog.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DogService } from '../services/dog.service';
+import { Dog } from '../models/dog';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-show-dog',
   templateUrl: './show-dog.component.html',
   styleUrls: ['./show-dog.component.css']
 })
-export class ShowDogComponent {
+export class ShowDogComponent implements OnInit {
+  dog: Dog = new Dog()
 
+  constructor(private route:ActivatedRoute,private dogservice: DogService){
+    console.log('entered constructor' + this.dog.name) //inadd
+    
+
+  }
+  ngOnInit(): void {
+    console.log('entered oninit' + this.dog.name)
+    this.route.params.forEach((params: Params) => {
+      if(params['id'] !== undefined){
+        const id = params['id'];
+        console.log('Id' + id)
+        this.dogservice.getDog(id).subscribe(data => {this.dog =data});
+      }
+    });
+  }
+    
 }
