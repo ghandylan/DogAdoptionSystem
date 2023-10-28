27b576735a4fe51cdffa61c1a84b71723f0f91d4
JaiTolentino <jaitolentinoo@gmail.com>
2023-10-28 18:45:31 +0800

    feat: fixed routing and endpoints

--- frontend/app/src/app/admin-edit-dog/admin-edit-dog.component.ts
+++ frontend/app/src/app/admin-edit-dog/admin-edit-dog.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Dog } from '../models/dog';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { DogService } from '../services/dog.service';
+import { VetService } from '../services/vet.service';
+import { AdminSessionServiceService } from '../services/admin-session-service.service';
 
 @Component({
   selector: 'app-admin-edit-dog',
@@ -6,5 +11,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-edit-dog.component.css']
 })
 export class AdminEditDogComponent {
+  selectedDog: Dog = new Dog();
+  dogs: Dog[] = [];
+  constructor(private router:Router, private route: ActivatedRoute, private dogservice: DogService,private vetService: VetService,private session: AdminSessionServiceService){
+  }
+  ngOnInit(): void {
+    if(this.session.getAccess() === ""){
+      this.router.navigate(['/login']);
+    }
+    this.dogservice.getDogs().subscribe((data: Dog[]) => {this.dogs = data});
 
+    this.route.params.forEach((params: Params) => {
+      if(params['id'] !== undefined){
+        const id = params['id'];
+        console.log('Id' + id)
+        this.dogservice.getDogById(id).subscribe(data => {this.selectedDog =data});
+      }
+    });
+  }
 }
